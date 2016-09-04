182fbfda52adb7dc35e9d896b0e6e3af13beaecf
Shaojung <shaojung@gmail.com>
2016-09-04 16:23:38 +0800

    Finish Read.

--- app/src/main/java/tw/com/pcschool/t090405/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t090405/MainActivity.java
@@ -2,12 +2,17 @@ package tw.com.pcschool.t090405;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,6 +41,27 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+    }
+
+    public void clickRead(View v)
+    {
+        char[] buffer = new char[1];
+        FileReader fr = null;
+        StringBuilder sb = new StringBuilder();
+        File file = new File(getFilesDir() + "/" + "mydata.txt");
+
+        try {
+            fr = new FileReader(file);
+            while (fr.read(buffer)!= -1) {
+                sb.append(new String(buffer));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.d("READFILE", sb.toString());
 
     }
 }
